d6c2e4c7d4b3cae485e895ae62524c1b243c9652
David Delfaus <ddelfaus@gmail.com>
2023-08-25 13:26:36 -0400

    knex

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express'); // import the express package
+
+const server = express(); // creates the server
+
+// handle requests to the root of the api, the / route
+server.get('/', (req, res) => {
+  res.send('Hello from Express');
+});
+
+// watch for connections on port 9000
+// server.listen(9000, () =>
+//   console.log('Server running on http://localhost:9000')
+// );
+
+
+
+
+
+module.exports = server;
